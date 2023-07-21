b1c684fba54d2636265a9d6bcfa79a151b5792e6
Allison Bailey <abailey@routeware.com>
2023-07-21 15:05:22 -0700

    Added in simple app into index.ts and pnpm build/start to run it.  Could NOT get local host 8080 to work.  But it ran internally and gave me Hello World

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
 "use strict";
-console.log("Hello, World");
+console.log("Hello, World"); //This is a test of committing changes via git
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8sIFdvcmxkJyk7XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IjtBQUFBQSxRQUFRQyxHQUFHLENBQUMifQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8sIFdvcmxkJyk7XG4vL1RoaXMgaXMgYSB0ZXN0IG9mIGNvbW1pdHRpbmcgY2hhbmdlcyB2aWEgZ2l0XG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJtYXBwaW5ncyI6IjtBQUFBQSxRQUFRQyxHQUFHLENBQUMsaUJBQ1osOENBQThDIn0=
